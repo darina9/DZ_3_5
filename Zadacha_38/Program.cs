46f7e9639c7e2dba43e47391013bdc989d8de013
darina9 <krastinadv@gmail.com>
2022-09-05 15:16:41 +0300

    Zadacha_38

---
+++ Zadacha_38/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] array = new double[5];
+  for (int i = 0; i < array.Length; i++ )
+  {
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+  }
+
+double max = array[0];
+double min = array[0];
+
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (max < array[i])
+    {
+      max = array[i];
+    }
+        if (min > array[i])
+    {
+      min = array[i];
+    }
+  }
+
+  double decision = max - min;
+
+  Console.WriteLine($"\nразница между между максимальным ({max}) и минимальным({min}) элементами: {decision}");
