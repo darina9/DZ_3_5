17f1705a89381a1f44b34541354f4a6740db96b1
darina9 <krastinadv@gmail.com>
2022-09-05 14:35:16 +0300

    Zadacha_36

---
+++ Zadacha_36/Program.cs
@@ -0,0 +1,22 @@
+﻿ int[]array = GetArray(4);
+Console.WriteLine(string.Join("," , array));
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for(int i = 0;i<size;i++)
+    {
+        result [i] = new Random().Next(-100,100);
+    }
+    return result;
+}
+int size= array.Length;
+int sum=0;
+for(int i = 0;i<size;i++)
+{  
+    
+    if(i%2==0)
+    {
+        sum+=array[i];
+    } 
+}
+Console.WriteLine("Сумма  элементов, стоящих на нечётных позициях :"+ sum);
