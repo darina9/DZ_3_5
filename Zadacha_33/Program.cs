7af0e8cc1c3af1bfb9cf9a4f570374bd95347c69
darina9 <krastinadv@gmail.com>
2022-09-05 13:36:44 +0300

    Zadacha_34

---
+++ Zadacha_33/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = new int[4];
+int size = array.Length;
+
+int count= 0;
+
+
+for(int i=0;i<size;i++)
+{
+   array[i]=new Random().Next(100,1000);
+   if(array[i]%2==0)
+   {
+     count++;
+   }
+}
+Console.WriteLine(String.Join(",", array));
+Console.WriteLine("Количество четных чисел " + count );
